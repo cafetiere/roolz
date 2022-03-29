2d3fb47b276b5106e87b7c94bed14397987671a9
cafetiere <asuresh@gocardless.com>
2022-03-29 16:24:36 +0100

    add autorequire functionality

--- lib/r00lz.rb
+++ lib/r00lz.rb
@@ -22,6 +22,24 @@ module R00lz
     end
   end
 
+  #used to conver class names to snake case to require files for autorequire
+  def self.to_underscore(s)
+    s.gsub(
+      /([A-Z]+)([A-Z][a-z])/,
+      '\1_\2').gsub(
+      /([a-z\d])([A-Z])/,
+      '\1_\2').downcase
+  end
+
+  # used for auto require
+  class Object
+    def self.const_missing(c)
+      require R00lz.to_underscore (c.to_s)
+      Object.const_get(c)
+    end
+  end
+
+
   class Controller
     attr_reader :env
 
